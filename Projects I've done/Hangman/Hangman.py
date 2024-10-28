2585df9edc4fc1e29689b7d8a50209306faa32ca
Remy <severinremy@hotmail.fr>
2024-10-28 02:06:34 +0100

    finished Hangman with no exterior help

--- Projects I've done/Hangman/Hangman.py
+++ Projects I've done/Hangman/Hangman.py
@@ -1,4 +1,5 @@
 import random
+import sys
 wordtoguess = []
 amountoftries = 5
 words = ["Aardvark", "Orange", "Mptkxp"]
@@ -8,17 +9,23 @@ selectedword = random.choice(words).upper()
 print (selectedword)
 for letter in selectedword:
     wordtoguess.append("_")
-x = "".join(wordtoguess)
 
 while amountoftries > 0:
 
     guessedletter = input(f"Enter your guessed letter, you have {amountoftries} left >")
+    if guessedletter not in selectedword:
+        amountoftries -= 1
+        print(f"You missed, you now have {amountoftries} lives left")
+    
+    for y in range(len(selectedword)):
+        letter = selectedword[y]
+        if letter is guessedletter:
+            wordtoguess[y] = guessedletter
+        if "_" not in wordtoguess:
+            print(f"Congratulations, the word is {selectedword} !")
+            sys.exit()   
+            
+    x = "".join(wordtoguess)
+    print(x)
 
-    for letter in selectedword:
-
-        if letter == guessedletter:
-            wordtoguess[selectedword.index(guessedletter)] = guessedletter
-    print(wordtoguess)
-        
-
-print(wordtoguess)
+print(f"No more lives left, the word was {selectedword}")
